4d0db59a390a2036fe838fdcd09ad8dafe3c1447
Dperaltat <peraltadaniel069@gmail.com>
2019-05-23 13:43:53 -0500

    probando

--- database/conexionBD.php
+++ database/conexionBD.php
@@ -3,7 +3,7 @@
  $db_servername = "localhost";
  $db_username = "root";
  $db_password = "";
- $db_name = "correo_electronico";
+ $db_name = "Correo_Electronico";
 
  $conn = new mysqli($db_servername, $db_username, $db_password, $db_name);
  $conn->set_charset("utf8");
